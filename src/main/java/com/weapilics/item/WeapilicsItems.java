8af4a183ec4e6b17fe01d4ef601bd07b31c5d37e
Falthera <falthera@users.noreply.github.com>
2026-04-15 12:49:11 +1000

    Add Void Trident and automate release versioning

--- src/main/java/com/weapilics/item/WeapilicsItems.java
+++ src/main/java/com/weapilics/item/WeapilicsItems.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.MaceItem;
+import net.minecraft.item.TridentItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -13,6 +14,21 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 
 public final class WeapilicsItems {
+	public static final Identifier VOID_TRIDENT_ID = Identifier.of(WeapilicsMod.MOD_ID, "void_trident");
+	public static final RegistryKey<Item> VOID_TRIDENT_KEY = RegistryKey.of(RegistryKeys.ITEM, VOID_TRIDENT_ID);
+	public static final Item VOID_TRIDENT = Registry.register(
+		Registries.ITEM,
+		VOID_TRIDENT_ID,
+		new VoidTridentItem(
+			new Item.Settings()
+				.registryKey(VOID_TRIDENT_KEY)
+				.attributeModifiers(TridentItem.createAttributeModifiers())
+				.maxCount(1)
+				.maxDamage(500)
+				.rarity(Rarity.EPIC)
+		)
+	);
+
 	public static final Identifier ROSEFALL_ID = Identifier.of(WeapilicsMod.MOD_ID, "rosefall");
 	public static final RegistryKey<Item> ROSEFALL_KEY = RegistryKey.of(RegistryKeys.ITEM, ROSEFALL_ID);
 	public static final Item ROSEFALL = Registry.register(
@@ -32,6 +48,9 @@ public final class WeapilicsItems {
 	}
 
 	public static void register() {
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> entries.add(ROSEFALL.getDefaultStack()));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+			entries.add(VOID_TRIDENT.getDefaultStack());
+			entries.add(ROSEFALL.getDefaultStack());
+		});
 	}
 }
