6add136fd8fe77dafb409d82521431ada3a607ce
Falthera <falthera@users.noreply.github.com>
2026-04-15 12:16:49 +1000

    tune: set Rosefall regen to 1s and 2-heart hit heal

--- src/main/java/com/weapilics/item/RosefallItem.java
+++ src/main/java/com/weapilics/item/RosefallItem.java
@@ -3,11 +3,18 @@ package com.weapilics.item;
 import com.weapilics.enchantment.RosefallEnchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MaceItem;
 import net.minecraft.server.world.ServerWorld;
 
 public class RosefallItem extends MaceItem {
+	public static final int REGEN_DURATION_TICKS = 20;
+	public static final int REGEN_AMPLIFIER = 0;
+	public static final float HIT_HEAL_AMOUNT = 4.0f;
+
 	public RosefallItem(Settings settings) {
 		super(settings);
 	}
@@ -23,5 +30,12 @@ public class RosefallItem extends MaceItem {
 	public void inventoryTick(ItemStack stack, ServerWorld world, Entity entity, EquipmentSlot slot) {
 		super.inventoryTick(stack, world, entity, slot);
 		RosefallEnchantments.ensureRosefallEnchantments(stack, world.getRegistryManager());
+	}
+
+	@Override
+	public void postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+		super.postHit(stack, target, attacker);
+		attacker.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, REGEN_DURATION_TICKS, REGEN_AMPLIFIER));
+		attacker.heal(HIT_HEAL_AMOUNT);
 	}
 }
