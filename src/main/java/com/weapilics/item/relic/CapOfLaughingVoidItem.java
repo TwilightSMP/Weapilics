cf43f97d788c45c07030de4f542a6f6e22f82c6e
Falthera <falthera@users.noreply.github.com>
2026-04-16 15:54:18 +1000

    Update CapOfLaughingVoidItem and add new textures

--- src/main/java/com/weapilics/item/relic/CapOfLaughingVoidItem.java
+++ src/main/java/com/weapilics/item/relic/CapOfLaughingVoidItem.java
@@ -1,12 +1,30 @@
 package com.weapilics.item.relic;
 
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-
+import net.minecraft.server.world.ServerWorld;
 
 public class CapOfLaughingVoidItem extends RelicItem {
-	public CapOfLaughingVoidItem(Settings settings) {
-		super(settings);
-	}
+    public static final int INVISIBILITY_DURATION = 5; // Duration in ticks
 
-	
+    public CapOfLaughingVoidItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void onTick(ServerWorld world, PlayerEntity player, ItemStack stack, EquipmentSlot slot) {
+        // Apply invisibility effect when the player is sneaking
+        if (player.isSneaking()) {
+            player.addStatusEffect(new StatusEffectInstance(
+                StatusEffects.INVISIBILITY,
+                INVISIBILITY_DURATION,
+                0,
+                false, // No particles
+                false  // No icon
+            ));
+        }
+    }
 }
